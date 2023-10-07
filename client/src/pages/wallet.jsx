efc973ca7fd7749538a4d197beda1ee53f2a32e9
ph-harikishan <ph-harikishan@mobiloitte.com>
2023-10-07 14:13:38 +0530

    h1

--- client/src/pages/wallet.jsx
+++ client/src/pages/wallet.jsx
@@ -4,8 +4,8 @@ import Web3 from "web3";
 import ABI from "./ABI.json";
 import { useNavigate } from 'react-router-dom';
 
-const Wallet = ({saveState}) => {
-    const navigateTo = useNavigate();
+const Wallet = ({ saveState }) => {
+  const navigateTo = useNavigate();
   const connectwallet = async () => {
     try {
       if (window.ethereum) {
@@ -15,8 +15,8 @@ const Wallet = ({saveState}) => {
         });
 
         const contractAddress = "0xbec24e5abd6daa586ceb4ec18bf4d94b6bc0bbcc";
-        const contract = new web3.eth.Contract(ABI,contractAddress);
-        saveState({web3:web3,contract:contract,account:accounts[0]})
+        const contract = new web3.eth.Contract(ABI, contractAddress);
+        saveState({ web3: web3, contract: contract, account: accounts[0] })
         navigateTo("/view-all-tasks")
       } else {
         throw new Error("Ethereum provider not available.");
@@ -28,19 +28,19 @@ const Wallet = ({saveState}) => {
 
   return (
     <>
-    <div className="wallet_header">
-      <span>Welcome To</span> <p>TODO 3.0</p>
+      <div className="wallet_header">
+        <span>Welcome To</span> <p>TODO 3.0</p>
       </div>
       <div className='connect_wallet_section todo_btn'>
         <p>Please Connect Metamask Wallet To Access The App</p>
-    <button onClick={() => connectwallet()}>Connect Wallet</button>
-    </div> 
+        <button onClick={() => connectwallet()}>Connect Wallet</button>
+      </div>
     </>
   );
 };
 
 Wallet.propTypes = {
-    saveState: PropTypes.func.isRequired,
+  saveState: PropTypes.func.isRequired,
 };
 
 export default Wallet;
